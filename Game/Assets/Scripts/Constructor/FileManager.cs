49bc9b43f00827db0a897f98a1e61fcc928b971b
Wyzzus <wyzzus@gmail.com>
2019-10-17 22:37:53 +0300

    #5 Add creating/editing locations image

---
+++ Game/Assets/Scripts/Constructor/FileManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SFB;
+
+public class FileManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
