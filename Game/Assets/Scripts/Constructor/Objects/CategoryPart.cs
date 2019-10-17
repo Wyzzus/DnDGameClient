a0dbdc384353ba3a89bd800a5d83995825539799
Wyzzus <wyzzus@gmail.com>
2019-10-18 00:57:14 +0300

    #4 Add creating/editing dnd objects

---
+++ Game/Assets/Scripts/Constructor/Objects/CategoryPart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CategoryPart : MonoBehaviour
+{
+    public Text Name;
+    public string Category;
+
+    public void Setup(string Name)
+    {
+        this.Category = Name;
+        this.Name.text = Name;
+    }
+
+    public void Delete()
+    {
+        PackConstructor.instance.ObjectsCategories.Remove(this.Category);
+        PackConstructor.instance.DndObjectEditorWindow.GetComponentInChildren<DndObjectEditor>().UpdateCategories();
+        Destroy(gameObject);
+    }
+}
---
+++ Game/Assets/Scripts/Constructor/Objects/PartDndObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartDndObject : MonoBehaviour
+{
+    public DndObject MyDndObject;
+    public Text Name;
+
+    public void EditObject()
+    {
+        DndObjectEditor editor = PackConstructor.instance.DndObjectEditorWindow.GetComponent<DndObjectEditor>();
+        PackConstructor.instance.OpenDndObjectEditor();
+        editor.EditObject(MyDndObject, this);
+    }
+
+    public void Delete()
+    {
+        PackConstructor.instance.DndObjects.Remove(MyDndObject);
+        var instance = PackConstructor.instance;
+        instance.RecalculateViewSize(instance.DndObjectView, instance.DndObjects.Count, 0, instance.LocationPartHeight);
+        Destroy(gameObject);
+    }
+
+    public void UpdateDndObject()
+    {
+        Name.text = MyDndObject.DndObjectName;
+    }
+}
