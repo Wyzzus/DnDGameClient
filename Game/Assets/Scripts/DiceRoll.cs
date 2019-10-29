50c05b707be718def4e6bf8f6a466cb6f0334ba0
fominykhartur <fominykhartur+GitHub@gmail.com>
2019-10-29 13:32:16 +0300

    test commit

---
+++ Game/Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoll : MonoBehaviour
+{
+    public Text Result;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Roll(int n)
+    {
+        Result.text = "Бросаем кубик ";
+        StopAllCoroutines();
+        StartCoroutine(DelayedRoll(n));
+    }
+
+    IEnumerator DelayedRoll(int n)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Result.text += ".";
+            yield return new WaitForSeconds(0.2F);
+        }
+    }
+}
