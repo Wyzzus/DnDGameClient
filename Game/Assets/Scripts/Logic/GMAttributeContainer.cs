4acbdf1817ec1e250d4adb1c4febb33e38128d3a
Wyzzus <wyzzus@gmail.com>
2019-12-19 23:33:22 +0300

    #34 Add Game Master functional

---
+++ Game/Assets/Scripts/Logic/GMAttributeContainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GMAttributeContainer : Container
+{
+    public Attribute MyAttribute;
+    public PlayerController GM;
+
+    public Text Value; 
+
+    public void Setup(Attribute at)
+    {
+        MyAttribute = at;
+        Name.text = MyAttribute.AttributeName;
+        Value.text = MyAttribute.AttributeValue.ToString();
+    }
+
+    public void Increase()
+    {
+        MyAttribute.AttributeValue += 0.5f;
+        Value.text = MyAttribute.AttributeValue.ToString();
+    }
+
+    public void Decrease()
+    {
+        MyAttribute.AttributeValue -= 0.5f;
+        Value.text = MyAttribute.AttributeValue.ToString();
+    }
+}
---
+++ Game/Assets/Scripts/Logic/GMItemContainer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMItemContainer : Container
+{
+    public PlayerController GM;
+    public PlayerController player;
+    public int MyItem;
+    public bool Equiped;
+
+    public void Setup(PlayerController GM, PlayerController player, int n, bool equiped)
+    {
+        MyItem = n;
+        this.GM = GM;
+        this.player = player;
+        Equiped = equiped;
+        string eq = (Equiped) ? " (e)" : "";
+        Name.text = GM.CurrentThemePack.Items[n].ItemName + eq;
+    }
+
+    public void DeleteItem()
+    {
+        player.RemoveItem(MyItem);
+        GM.ShowPlayerInfo();
+    }
+
+    public void AddItem()
+    {
+        player.AddItem(MyItem);
+        GM.ShowPlayerInfo();
+    }
+}
